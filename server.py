ca6f51de77efffad0808ffc062eea8566640fa0a
Janson <yvrjansonlin@gmail.com>
2019-01-26 09:51:27 -0800

    [resume] initial repo

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+@app.route('/resume')
+def resume():
+	return render_template('Resume.html')
+
+if __name__ == '__main__':
+	app.run()
